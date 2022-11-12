d2dee19faa7a0386cd394672a6f9cf830d74104d
MUHAMAMDWASIF <wasifmemon30888@gmail.com>
2022-11-13 03:27:06 +0500

    first commit

---
+++ app3.js
@@ -0,0 +1,32 @@
+const btn = document.querySelectorAll(".play");
+// var score = 0;
+console.log(btn);
+var timeLeft = 10;
+    var elem = document.getElementById('timer');
+    
+    
+    function countdown() {
+      if (timeLeft <= -1) {
+        clearTimeout(timerId);
+        window.location.href="timer.html"
+      }else if (timeLeft<10) {
+       elem.innerHTML= `0${timeLeft--}`
+      }      
+      else {
+        elem.innerHTML = timeLeft--;
+      }
+      console.log(timeLeft);
+    }
+    var timerId = setInterval(countdown, 1000);
+function ans(e) {
+  let score;
+  if (e.target.innerText==="background-repeat") {
+    window.location.href="qfour.html"
+  }else{
+    window.location.href="wrong.html";
+  }
+}
+    for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click',ans)
+  
+}
---
+++ timer.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById("play");
+btn.addEventListener('click',play)
+function play() {
+    window.location.href="index.html"
+}
